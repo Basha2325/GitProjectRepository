2c014885a8fba37281d7a756e89764b325e34518
Basha2325 <rbeee08@gmail.com>
2021-02-18 00:17:21 +0530

    per2 code done

--- src/GitRepository/Sample.java
+++ src/GitRepository/Sample.java
@@ -4,6 +4,11 @@ public class Sample {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("Person 2 added some lines of code");
+		
+		System.out.println("Person 2 completed coding");
+		
+		
 		
 	}
 
